96d1da6a477e65a886946095069cc38cc9fc0d6b
Thomas May <thomasmay98@outlook.com>
2019-02-08 10:49:07 +0000

    Created player controller

---
+++ Assets/MayThomas/Scripts/flocking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flocking : MonoBehaviour {
+
+	
+
+	void Start () {
+		
+	}
+	void Update () {
+
+        Debug.Log(GetComponent<Rigidbody>().velocity);
+	}
+
+    void calcVelocity()
+    {
+
+    }
+}
